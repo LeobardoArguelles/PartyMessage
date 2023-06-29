7da0a1fd3a6822741df16d0f7827b4e67263ef6e
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2023-06-28 20:07:13 -0700

    Initial Commit

---
+++ PartyMessage/PartyMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyMessageApp.swift
+//  PartyMessage
+//
+//  Created by Leobardo Arguelles on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
